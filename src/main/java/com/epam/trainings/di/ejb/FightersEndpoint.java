577a10b39707d86f8dd8893c3fedf1863bcaf1ad
Anuar Nurmakanov <anuar_nurmakanov@epam.com>
2016-10-07 15:53:39 +0600

    Added ejb configuration

--- src/main/java/com/epam/trainings/di/ejb/FightersEndpoint.java
+++ src/main/java/com/epam/trainings/di/ejb/FightersEndpoint.java
@@ -1,5 +1,6 @@
 package com.epam.trainings.di.ejb;
 
+import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,12 +9,12 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/")
 public class FightersEndpoint {
+    @EJB
+    private FightersService fightersService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String hello() {
-
         return "hello: wildfly swarm + gradle + java";
     }
-
 }
